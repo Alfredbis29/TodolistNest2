26395dfc85ec8dadd62470bb388bffc82f227b1d
Alfredbis29 <kalumunabisimwa5@gmail.com>
2025-02-28 01:39:56 +0200

    Add Tailwind to style the app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./views/**/*.{html,ejs,hbs}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
